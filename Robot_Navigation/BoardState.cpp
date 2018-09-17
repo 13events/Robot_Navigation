d958155ffc4944f07886335cd00707974bcf05f3
13events <jdmartinez90@protonmail.com>
2018-09-16 22:15:19 -0700

    adjusted table spacing made instructions clearer.

--- Robot_Navigation/BoardState.cpp
+++ Robot_Navigation/BoardState.cpp
@@ -59,7 +59,7 @@ void BoardState::printBoard()
 
 	for (int i = 0; i < boardHeight; i++) {
 		for (int j = 0; j < boardWidth; j++) {
-			std::cout << boardMatrix[i][j] << " ";
+			std::cout << boardMatrix[i][j] << "  ";
 		}
 		std::cout << "\n";
 	}
